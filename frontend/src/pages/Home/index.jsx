6cd267404eff8431c4ade45dbfc981ea2c16afa5
mayrazan <mayramanchein@yahoo.com.br>
2021-05-17 20:00:58 -0300

    Creating menu

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+// import Header from "../../components/Header";
+import MenuDrawer from "../../components/Menu";
+
+const Home = () => {
+  return (
+    <>
+      <MenuDrawer />
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/styles/index.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+import { colors } from "./colors";
+
+export const GlobalStyle = createGlobalStyle`
+
+*, body{
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  list-style: none;
+  vertical-align: baseline;
+  outline: 0!important;
+  box-sizing: border-box;
+}
+
+body, html {
+  height: 100%;
+}
+
+body {
+  background-color: ${colors.white};
+  color: ${colors.text};
+  font-size: 16px;
+  font-family: Roboto, Arial, Helvetica, sans-serif;
+}
+
+button {
+  background: transparent;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+#root {
+  height: 100%;
+}
+`;
